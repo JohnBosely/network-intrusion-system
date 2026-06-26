1e6367ddbe786bb413b4cb2d7283f803a2cc70d9
Ojei David <davidojei75@gmail.com>
2026-06-26 23:48:06 +0100

    FINAL DOCKERIZATION I THINK

--- dashboard/streamlit_app.py
+++ dashboard/streamlit_app.py
@@ -263,7 +263,7 @@ def main():
 
         st.divider()
         st.caption("CICIDS2017 Dataset Simulation")
-        st.caption("Three-Tier Autonomous Defense")
+        st.caption("Three-Tier Autmmonomous Defense")
 
     # ---- Header ----
     st.title("🛡️ Autonomous Network Intrusion Detection System")
